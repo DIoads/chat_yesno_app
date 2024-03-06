4405611795b763d494f05ea9b5985f936c1438f3
DIoads <jose.alejandro.guzman156@gmail.com>
2024-03-05 23:28:53 -0800

    05-Marzo-2024 Cambios a cheems_message_bubble

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -29,20 +29,21 @@ class _chatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        children: [
-          Expanded(
-              child: ListView.builder(
-                  itemCount: 101,
-                  itemBuilder: ((context, index) {
-                    return (index % 2 == 0)
-                        ? const MyMessageBubble()
-                        : const CheemsMessageBubble();
-                  }))),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 101,
+                    itemBuilder: ((context, index) {
+                      return (index % 2 == 0)
+                          ? const MyMessageBubble()
+                          : const CheemsMessageBubble();
+                    }))),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/presentation/widgets/cheems_message_bubble.dart
+++ lib/presentation/widgets/cheems_message_bubble.dart
@@ -21,9 +21,43 @@ class CheemsMessageBubble extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 20,
+          height: 10,
+        ),
+        const _ImageBubble(),
+        const SizedBox(
+          height: 10,
         )
       ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  const _ImageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    print("ancho ${size.width}=${size.width * 0.7}");
+    print("alto ${size.height}=${size.height * 0.1}");
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        width: size.width * 0.4,
+        height: size.height * 0.2,
+        fit: BoxFit.cover,
+        'https://www.coca-cola.com/content/dam/onexp/mx/es/brands/coca-cola/coca-cola-original/Product-Information-Section-Coca-Cola-Original.jpg',
+        loadingBuilder: (context, child, loadingProgress) =>
+            (loadingProgress == null)
+                ? child
+                : Container(
+                    width: size.width * 0.4,
+                    height: size.height * 0.2,
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: const Text("Cargando imagen ..."),
+                  ),
+      ),
     );
   }
 }
