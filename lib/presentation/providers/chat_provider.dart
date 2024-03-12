e3258333aff2607b3316978f511ae50fb8f19c93
DIoads <jose.alejandro.guzman156@gmail.com>
2024-03-12 00:04:47 -0700

    11-Marzo-2024 Fixed null check error

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
   List<Message> messageList = [
-    Message(text: "hello bro", fromWho: FromWho.cheems),
+    Message(
+        text: "hello bro",
+        fromWho: FromWho.cheems,
+        imageURL:
+            "https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg"),
     Message(text: "whats up my pa", fromWho: FromWho.me),
     Message(
         text: "funny meme",
         fromWho: FromWho.cheems,
         imageURL:
-            'https://www.coca-cola.com/content/dam/onexp/mx/es/brands/coca-cola/coca-cola-original/Product-Information-Section-Coca-Cola-Original.jpg'),
+            "https://www.coca-cola.com/content/dam/onexp/mx/es/brands/coca-cola/coca-cola-original/Product-Information-Section-Coca-Cola-Original.jpg"),
     Message(text: "haha", fromWho: FromWho.me)
   ];
 
